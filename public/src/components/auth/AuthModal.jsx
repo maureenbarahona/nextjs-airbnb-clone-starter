379d1589012a431cda8d98e9b272a54a2b3e0932
Maureen Barahona <maureenbarahona@gmail.com>
2024-02-18 20:55:07 -0600

    Async functions

--- public/src/components/auth/AuthModal.jsx
+++ public/src/components/auth/AuthModal.jsx
@@ -10,16 +10,20 @@ const AuthModal = () => {
   const [lastName, setLastName] = useState("");
   const [userFound, setUserFound] = useState(null);
 
-  const verifyEmail = async () => {
-    
-  }
+  const verifyEmail = async () => {};
+  const handleLogin = async () => {};
+  const handleSignup = async () => {};
 
   return (
     <div className="relative z-50">
       <div className="fixed inset-0 bg-gray-500 opacity-75 transition-opacity"></div>
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-x1 transition-all">
+          <div
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-x1 transition-all
+          sm:my-8 sm:w-full sm:max-w-lg
+          "
+          >
             <div className="bg-white pb-4 pt-5">
               <div className="border-b border-b-gray-200 flex items-center justify-center relative pb-5">
                 <span className="absolute left-5 cursor-pointer text-lg">
