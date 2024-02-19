04a5cff56c4197ca18d946ed1009eae60c7ffde9
Maureen Barahona <maureenbarahona@gmail.com>
2024-02-18 18:05:38 -0600

    add contextMenu

--- public/src/components/common/ContextMenu.jsx
+++ public/src/components/common/ContextMenu.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const ContextMenu = () => {
-  return <div>ContextMenu</div>;
+const ContextMenu = ({ options, cordinates, contextMenu, setContextMenu }) => {
+  const contextMenuRef = useRef(null);
+
+  return (
+    <div
+      ref={contextMenuRef}
+      style={{
+        boxShadow:
+        "0 2px 5px 0 rgba(var(11,20,26),.26),0 2px 10px 0 rgba(11,20,26;),.16",
+        top:cordinates.y,
+        left:cordinates.x,
+      }}
+      className="bg-white shadow-2x1 fixed py-5 z-[100] rounded-lg border border-gray-200"
+    >
+      <ul>
+        {
+          options.map(({name,callback})=> <li className="hover:bg-gray-100 pl-5 pr-10 py-2 cursor-ponter" key={name}>
+            <span>{name}</span>
+          </li>)
+        }
+      </ul>
+    </div>
+  );
 };
 
 export default ContextMenu;
--- public/src/components/navbar/Navbar.jsx
+++ public/src/components/navbar/Navbar.jsx
@@ -1,11 +1,40 @@
-
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import AirbnbLogo from "airbnb/svg/airbnb-logo";
 import { FiGlobe } from "react-icons/fi";
 import { RxHamburgerMenu } from "react-icons/rx";
+import ContextMenu from "../common/ContextMenu";
 
 const Navbar = () => {
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+
+  const contextMenuOptions = [
+    {
+      name: "Login",
+      callBack: () => {
+        setIsContextMenuVisible(false);
+      },
+    },
+    {
+      name: "Signup",
+      callBack: () => {
+        setIsContextMenuVisible(false);
+      },
+    },
+    {
+      name: "AirBnb your home",
+      callBack: () => {
+        setIsContextMenuVisible(false);
+      },
+    },
+    {
+      name: "help",
+      callBack: () => {
+        setIsContextMenuVisible(false);
+      },
+    },
+  ];
   return (
     <header className="w-full flex flex-col justify-center transition-all duration-300 h-20 border-b border-b-gray-200">
       <div className="flex items-center justify-between px-20">
@@ -22,19 +51,34 @@ const Navbar = () => {
             <li className="cursor-pointer">
               <FiGlobe />
             </li>
-            <li className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-x1 transition-all duration-500">
+            <li
+              className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-x1 transition-all duration-500 "
+              onClick={() => setIsContextMenuVisible(true)}
+            >
               <RxHamburgerMenu />
               <span>
                 <Image
                   src="/empty-profile.png"
                   alt="profile"
                   height={30}
-                  width={30} />
+                  width={30}
+                />
               </span>
             </li>
           </ul>
         </div>
       </div>
+      {isContextMenuVisible && (
+        <ContextMenu
+          contextMenu={isContextMenuVisible}
+          setContextMenu={setIsContextMenuVisible}
+          cordinates={{
+            x: window.innerWidth - 250,
+            y: 70,
+          }}
+          options={contextMenuOptions}
+        />
+      )}
     </header>
   );
 };
